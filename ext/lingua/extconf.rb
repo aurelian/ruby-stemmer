183f2641fb2fdb0a0e442b896f6001cc4263a9fc
aurelian <oancea@gmail.com>
2009-10-28 23:29:17 +0100

    Possible fix for issue 1: gmake is make command on FreeBSD  http://github.com/aurelian/ruby-stemmer/issues#issue/1

--- ext/lingua/extconf.rb
+++ ext/lingua/extconf.rb
@@ -5,9 +5,10 @@ ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
 LIBSTEMMER = File.join(ROOT, 'libstemmer_c')
 
 # build libstemmer_c
+make= (RUBY_PLATFORM =~ /freebsd/)? 'gmake' : 'make'
 ENV['ARCHFLAGS']= "-arch #{Config::CONFIG['host_cpu']}" if RUBY_PLATFORM =~ /darwin/
 ENV['ARCHFLAGS']= "-arch x86_64" if Config::CONFIG['host_cpu'] == 'i686' && RUBY_PLATFORM =~ /darwin/
-system "cd #{LIBSTEMMER}; make libstemmer.o; cd #{ROOT};"
+system "cd #{LIBSTEMMER}; #{make} libstemmer.o; cd #{ROOT};"
 
 $CFLAGS  += " -I#{File.join(LIBSTEMMER, 'include')} "
 $libs    += " -L#{LIBSTEMMER} #{File.join(LIBSTEMMER, 'libstemmer.o')} "
