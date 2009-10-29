d6886a84ecf1af364449f9ac92d7de1548998123
aurelian <oancea@gmail.com>
2009-10-29 12:44:44 +0100

    A better detection of arch flags

--- ext/lingua/extconf.rb
+++ ext/lingua/extconf.rb
@@ -1,14 +1,25 @@
-ENV['RC_ARCHS'] = '' if RUBY_PLATFORM =~ /universal-darwin/
+ENV['RC_ARCHS'] = '' if RUBY_PLATFORM =~ /darwin/
 require "mkmf"
 
 ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
 LIBSTEMMER = File.join(ROOT, 'libstemmer_c')
 
 # build libstemmer_c
+# FreeBSD make is gmake
 make= (RUBY_PLATFORM =~ /freebsd/)? 'gmake' : 'make'
-ENV['ARCHFLAGS']= "-arch #{Config::CONFIG['host_cpu']}" if RUBY_PLATFORM =~ /darwin/
-ENV['ARCHFLAGS']= "-arch x86_64" if Config::CONFIG['host_cpu'] == 'i686' && RUBY_PLATFORM =~ /darwin/
+# MacOS architecture mess up
+if RUBY_PLATFORM =~ /darwin/
+  begin
+    ENV['ARCHFLAGS']= "-arch " + %x[file #{File.expand_path(File.join(Config::CONFIG['bindir'], Config::CONFIG['RUBY_INSTALL_NAME']))}].strip!.match(/executable (.+)$/)[1] unless ENV['ARCHFLAGS'].nil?
+  rescue
+    $stderr << "Failed to get your ruby executable architecture.\n"
+    $stderr << "Please specify one using $ARCHFLAGS environment variable.\n"
+    exit
+  end
+end
+# make this stuff
 system "cd #{LIBSTEMMER}; #{make} libstemmer.o; cd #{ROOT};"
+exit unless $? == 0
 
 $CFLAGS  += " -I#{File.join(LIBSTEMMER, 'include')} "
 $libs    += " -L#{LIBSTEMMER} #{File.join(LIBSTEMMER, 'libstemmer.o')} "
